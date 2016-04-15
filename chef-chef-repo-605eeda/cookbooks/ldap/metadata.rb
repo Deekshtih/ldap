d0ff6e652d69ed4710d545b1edbf431cbab0be3e
deekshith <deekshith207@gmail.com>
2016-04-15 16:49:52 -0400

    First commit

---
+++ chef-chef-repo-605eeda/cookbooks/ldap/metadata.rb
@@ -0,0 +1,10 @@
+name             'ldap'
+maintainer       'comcast'
+maintainer_email 'bdkumar207@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures ldap'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+supports 'ubuntu', ">= 10.04"
+supports 'debian', ">= 7.0.0"
